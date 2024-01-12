7cf3342ee67f2e30dda2840088667100c9057562
Immi01 <immanuel.ehe@gmx.at>
2024-01-12 14:46:53 +0100

    added onClick and better style

--- Webdev/ReactDemo/adventCalendar/src/Components/Window.js
+++ Webdev/ReactDemo/adventCalendar/src/Components/Window.js
@@ -1,6 +1,17 @@
 export default function Window({num}) {
+
+  /*function changeColor(WindowId) {
+    React.useEffect(() => {
+      document.getElementById(WindowId).classList.add("WindowContainerClicked");
+    }, [])
+  }*/
+
+  const callId = "Window" + num;
+
   return (
-    <div class="WindowContainer">
+    <div class="WindowContainer" id={callId} onClick={() => {
+      document.getElementById(callId).classList.add("WindowContainerClicked");
+    }}>
       <div class="Window">
        {num+1}
       </div>
