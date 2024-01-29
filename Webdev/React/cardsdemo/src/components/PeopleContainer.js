cc0170fba5141c6801dd10c5370fa49d6d284944
Immi01 <immanuel.ehe@gmx.at>
2024-01-29 08:47:32 +0100

    Added Search feature

--- Webdev/React/cardsdemo/src/components/PeopleContainer.js
+++ Webdev/React/cardsdemo/src/components/PeopleContainer.js
@@ -3,20 +3,32 @@ import Card from "./Card";
 
 export default function PeopleContainer() {
     const [people, setPeople] = useState([]);
+    const [filteredPeople, setFilteredPeople] = useState([]);
 
     useEffect(() => {
         fetch("https://65b0e87ad16d31d11bdd8cae.mockapi.io/swp/person").then(
             res => res.json().then(data=>{
                 setPeople(data);
+                setFilteredPeople(data);
             })
         );
     }, [])
 
+    const filterPeople = (filter) => {
+        let filtered = people.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()));
+        setFilteredPeople(filtered);
+    }
+
     return (
-        <div>
-            <h1>People</h1>
-            <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
-                {people.map(person => {
+        <div className="">
+            <div className="fixed border mb-4 bg-white w-full h-28 p-8">
+                <input type="text" className="border m-2" placeholder="Search" onChange={(el) => {
+                    console.log(el.target.value);
+                    filterPeople(el.target.value);
+                }}/>
+            </div>
+            <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 p-8 pt-32'>
+                {filteredPeople.map(person => {
                     return <Card name={person.name} imageUri={person.avatar} title={person.job}/>
                 })}
             </div>
