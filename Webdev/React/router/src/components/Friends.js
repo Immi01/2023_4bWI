75af6e702ff0dc5ba2ba585264b0721088057c5f
Immi01 <immanuel.ehe@gmx.at>
2024-02-28 11:22:01 +0100

    completed Router Pass Data

--- Webdev/React/router/src/components/Friends.js
+++ Webdev/React/router/src/components/Friends.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function  People() {
+    const location = useLocation();
     const navigate = useNavigate();
+
+    console.log(location.state);
     return <div>
         <h1>Friends</h1>
         <div onClick={() => {navigate("/");}}>go to People</div>
--- Webdev/React/router/src/components/People.js
+++ Webdev/React/router/src/components/People.js
@@ -4,6 +4,6 @@ import { Link } from "react-router-dom";
 export default function  People() {
     return <div>
         <h1>People</h1>
-        <Link to="friends">Friends</Link>
+        <Link to="friends" state={{name:"Hans", age:17}}>Friends</Link>
     </div>
 }
