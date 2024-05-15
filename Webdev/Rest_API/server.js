e640e4ce717d3c410e2bafd1259b232960611570
Immi01 <immanuel.ehe@gmx.at>
2024-05-15 10:23:39 +0200

    completed GET with id and DELETE

--- Webdev/Rest_API/server.js
+++ Webdev/Rest_API/server.js
@@ -22,6 +22,18 @@ app.get('/people', (req, res) => {
     res.send(data);
 });
 
+app.get('/people/:id', (req, res) => {
+    let id = req.params.id;
+    res.send(data[id]);
+});
+
+
+app.delete('/people/:id', (req, res) => {
+    let id = req.params.id;
+    data.splice(id,1);
+    res.send("done");
+});
+
 
 app.post('/post', (req, res) => {
     data.push(req.body);
